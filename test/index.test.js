23c8dcfc5606fe36b89ca719498ddeef8c1d9fed
Winton Welsh <mail@wintoni.us>
2018-02-08 01:35:49 -0500

    Discern b/w JSON and text

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,7 @@ import { fixtures } from "fxtr"
 test("dirToObj", async () => {
   let { path } = await fixtures(__dirname, "fixtures")
   let { obj } = await dirToObj(path)
+
   expect(obj).toEqual({
     bang: { buzz: { buzzValue: true } },
     fizz: { fizzValue: true },
@@ -15,7 +16,24 @@ test("dirToObj", async () => {
 test("set", async () => {
   let { path } = await fixtures(__dirname, "fixtures")
   let config = await getSet(path)
+
+  config = await config.set("text", "Some more text!")
+  expect(config.get("text")).toBe("Some more text!")
+
+  config = await config.set("bang.buzz", { hi: "hello" })
+  expect(config.get("bang.buzz")).toEqual({ hi: "hello" })
+})
+
+test("merge", async () => {
+  let { path } = await fixtures(__dirname, "fixtures")
+  let config = await getSet(path)
+
   config = await config.merge("bang.buzz", { hello: true })
+  expect(config.get("bang.buzz")).toEqual({
+    buzzValue: true,
+    hello: true,
+  })
+
   config = await config.merge("bang.buzz", {
     helloAgain: true,
   })
