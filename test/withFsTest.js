53d8ac235ca7df621bd90870c6fa7e892a1d4984
Winton Welsh <mail@wintoni.us>
2018-03-10 21:56:20 -0500

    More withFs tests

--- test/withFsTest.js
+++ test/withFsTest.js
@@ -1,4 +1,7 @@
+import { pathExists } from "fs-extra"
+import { resolve } from "path"
 import { fixtures } from "fxtr"
+
 import GetSet, { withFs } from "../dist"
 
 let path, read, store
@@ -28,5 +31,75 @@ test("set", async () => {
 
   expect(store.get("bang.buzz.nested")).toEqual({
     hello: "world",
+  })
+})
+
+test("set (concurrent)", async () => {
+  await Promise.all([
+    store.merge("fizz", { a: true }),
+    store.merge("fizz", { b: true }),
+    store.merge("fizz", { c: true }),
+  ])
+
+  expect(await read("fizz.json")).toEqual({
+    a: true,
+    b: true,
+    c: true,
+    fizzValue: true,
+  })
+})
+
+test("set (undefined)", async () => {
+  await store.set("text", undefined)
+  expect(store.get("text")).toBe(undefined)
+  expect(
+    await pathExists(resolve(path, "text.txt"))
+  ).not.toBeTruthy()
+})
+
+test("set (new prop)", async () => {
+  await store.set("newProp", "hey")
+
+  expect(store.get("newProp")).toBe("hey")
+
+  expect(
+    await pathExists(resolve(path, "newProp.json"))
+  ).toBeTruthy()
+
+  await store.set("bang.boom", { whoah: true })
+
+  expect(store.get("bang.boom")).toEqual({ whoah: true })
+
+  expect(
+    await pathExists(resolve(path, "bang/boom.json"))
+  ).toBeTruthy()
+})
+
+test("merge", async () => {
+  await store.merge("bang", { boom: true })
+
+  expect(store.get("bang")).toEqual({
+    boom: true,
+    buzz: {
+      buzzValue: true,
+    },
+  })
+
+  await store.merge("bang.buzz", { hello: {} })
+
+  expect(store.get("bang.buzz")).toEqual({
+    buzzValue: true,
+    hello: {},
+  })
+
+  await store.merge("bang.buzz.hello", {
+    helloAgain: true,
+  })
+
+  expect(store.get("bang.buzz")).toEqual({
+    buzzValue: true,
+    hello: {
+      helloAgain: true,
+    },
   })
 })
