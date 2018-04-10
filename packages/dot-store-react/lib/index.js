c435389347786e15658a4f0d95ace9f597b1b2d8
Winton Welsh <mail@wintoni.us>
2018-04-10 01:54:59 -0400

    Unsubscribe from store on componentWillUnmount

--- packages/dot-store-react/lib/index.js
+++ packages/dot-store-react/lib/index.js
@@ -32,13 +32,20 @@ export class StoreProvider extends React.Component {
     super(props)
     this.changes = []
     this.store = props.store
+    this.onUpdate = this.onUpdate.bind(this)
+  }
+
+  onUpdate(change) {
+    this.changes = this.changes.concat([change])
+    this.forceUpdate()
   }
 
   componentDidMount() {
-    this.store.subscribe(change => {
-      this.changes = this.changes.concat([change])
-      this.forceUpdate()
-    })
+    this.store.subscribe(this.onUpdate)
+  }
+
+  componentWillUnmount() {
+    this.store.unsubscribe(this.onUpdate)
   }
 
   render() {
